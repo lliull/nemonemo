51367c9321838633ccb2eaf6ee95d230ed6a6ed8
jinsoo <01026551290@naver.com>
2020-05-29 19:30:38 +0900

    vntl

--- toyStudy1/src/com/toyStudy/Main.java
+++ toyStudy1/src/com/toyStudy/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello word!!");
-        System.out.println("hello word!!");
-        System.out.println("hello word!!");
-        System.out.println("hello word!!");
-        System.out.println("두번째");
-        System.out.println("두번째 word!!");
-        System.out.println("두번째 word!!");
+        System.out.println("ddd");
     }
 }
