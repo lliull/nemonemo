49d61bfd4bdbbabc1250e858853338990676e502
jinsoo <01026551290@naver.com>
2020-06-15 22:01:11 +0900

    vuesutdy

---
+++ 0615/src/components/common.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h2{
+    color: blue;
+    font-weight: bold;
+  }
+</style>
---
+++ 0615/src/views/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <name />
+    <p>ddddd</p>
+  </div>
+</template>
+
+<script>
+import name from '@/components/common.vue'
+
+export default {
+  components: {
+    name
+  }
+}
+
+</script>
+
+<style>
+
+</style>
